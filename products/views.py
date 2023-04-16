8e79ff7ce2b882a326707ce29186c0ba5f9ed036
KertT12 <tammkert@gmail.com>
2023-04-16 16:02:02 +0300

    Shoppingpardise

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(requests):
+    context = {
+        'cart_entries': [
+            {
+                'title': 'GOOD BOOKS!',
+                'body': 'I have created my first template in Django!',
+
+
+            },
+            {
+                'title': 'ITEMS',
+                'body': 'LalalaLand',
+
+            },
+            {
+                'title': 'ANOTHER ONE!!',
+                'body': 'MAYBE?',
+
+            },  {
+                'title': 'SOMETHING !!',
+                'body': 'Am i doing it right?',
+
+            }, {
+                'title': 'ELSE!!',
+                'body': 'Am i doing it right?',
+
+            }
+        ]
+    }
+
+    return render(requests, 'home.html', context)
+
+
+
+def products(requests):
+    return render(requests, "products.html")
